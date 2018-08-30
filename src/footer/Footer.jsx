13f27a2aa644ffa58c436654d1bd109b887e875c
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-08-29 20:37:58 -0400

    Add copyright

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -14,9 +14,12 @@ const LeftBadge = styled('img')`
     height: 180px;
 `;
 
-const CenterContent = styled('div')`
+const CenterContainer = styled('div')`
+    display: flex;
     flex-grow: 1;
     flex-basis: 0;
+    font-family: 'Open Sans', sans-serif;
+    color: white;
 `;
 
 const RightLogo = styled('img')`
@@ -24,10 +27,18 @@ const RightLogo = styled('img')`
     align-self: center;
 `;
 
+const CenterContent = styled('div')`
+    flex-grow: 1;
+    align-self: center;
+    text-align: center;
+`;
+
 export default () => (
     <FooterContainer>
         <LeftBadge src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg" />
-        <CenterContent />
+        <CenterContainer>
+            <CenterContent>Copyright &copy;2018 HackDuke.</CenterContent>
+        </CenterContainer>
         <RightLogo src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-white.svg" />
     </FooterContainer>
 );

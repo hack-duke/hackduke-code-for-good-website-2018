3ac059731cc12ee816905b58db2387b21bed1796
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2018-09-13 09:19:53 -0500

    Switch App back to stateless component

--- src/App.jsx
+++ src/App.jsx
@@ -8,48 +8,44 @@ import FAQs from './faqs/FAQs';
 import Sponsors from './sponsors/Sponsors';
 import Footer from './footer/Footer';
 
-export default class App extends React.Component {
-    sections = [
-        {
-            title: 'Welcome',
-            titleColor: '#5c5299',
-            Component: Landing
-        },
-        {
-            title: 'About',
-            titleColor: '#3a3d6d',
-            Component: About
-        },
-        {
-            title: 'Tracks',
-            titleColor: '#5f76cb',
-            Component: Tracks
-        },
-        {
-            title: 'FAQs',
-            titleColor: '#f16a43',
-            Component: FAQs
-        },
-        {
-            title: 'Sponsors',
-            titleColor: '#f8aa5b',
-            Component: Sponsors
-        }
-    ];
-
-    render() {
-        return (
-            <React.Fragment>
-                <Navbar navItems={this.sections} />
-                {this.sections.map(({ title, titleColor, Component }) => (
-                    <Component
-                        id={title.toLowerCase()}
-                        key={title}
-                        titleColor={titleColor}
-                    />
-                ))}
-                <Footer />
-            </React.Fragment>
-        );
+const sections = [
+    {
+        title: 'Welcome',
+        titleColor: '#5c5299',
+        Component: Landing
+    },
+    {
+        title: 'About',
+        titleColor: '#3a3d6d',
+        Component: About
+    },
+    {
+        title: 'Tracks',
+        titleColor: '#5f76cb',
+        Component: Tracks
+    },
+    {
+        title: 'FAQs',
+        titleColor: '#f16a43',
+        Component: FAQs
+    },
+    {
+        title: 'Sponsors',
+        titleColor: '#f8aa5b',
+        Component: Sponsors
     }
-}
+];
+
+export default () => (
+    <React.Fragment>
+        <Navbar navItems={sections} />
+        {sections.map(({ title, titleColor, Component }) => (
+            <Component
+                id={title.toLowerCase()}
+                key={title}
+                titleColor={titleColor}
+            />
+        ))}
+        <Footer />
+    </React.Fragment>
+);

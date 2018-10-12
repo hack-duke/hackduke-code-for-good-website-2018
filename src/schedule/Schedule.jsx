35b130c050dd8c3d76b4fe5266fbe492f86b4992
Yunyu Lin <mail@yunyul.in>
2018-10-11 22:52:05 -0400

    Update schedule

--- src/schedule/Schedule.jsx
+++ src/schedule/Schedule.jsx
@@ -69,33 +69,23 @@ export default class Schedule extends React.Component {
         events: [
           {
             time: '9:00AM - 10:45AM',
-            name: 'Check-in',
+            name: 'Check-In',
             locations: ['Schiciano Atrium']
           },
           {
-            time: '11:00AM - 12:15PM',
+            time: '11:00AM - 12:30PM',
             name: 'Opening Ceremony',
             locations: ['Reynolds Theatre']
           },
           {
-            time: '12:30PM - 1:00PM',
-            name: 'Track Mentor Talks',
-            locations: [
-              'Scichiano A: Education',
-              'Scichiano B: Health',
-              'Hudson 207: Energy',
-              'Hudson 208: Inequality'
-            ]
-          },
-          {
             time: '1:00PM - 2:00PM',
             name: 'Lunch',
-            locations: ['CIEMAS 1st floor']
+            locations: ['Schiciano Atrium']
           },
           { time: '2:00PM', name: 'Hacking Commences' },
           {
             time: '2:00PM - 2:30PM',
-            name: 'Team Formation Help',
+            name: 'Team Formation Mixer',
             locations: ['CIEMAS']
           },
           {
@@ -104,14 +94,34 @@ export default class Schedule extends React.Component {
             locations: ['Harrington Quad']
           },
           {
-            time: '2:30PM - 6:30PM',
-            name: 'Workshops & Talks',
-            locations: ['Hudson']
+            time: '2:30PM - 3:30PM',
+            name: 'Arduino Workshop by August Ning',
+            locations: ['Hudson 201']
+          },
+          {
+            time: '2:30PM - 3:30PM',
+            name: 'VR Workshop by Mark Steelman',
+            locations: ['Hudson 232']
+          },
+          {
+            time: '3:00PM - 4:30PM',
+            name: 'Javascript Workshop by Co-Lab',
+            locations: ['Hudson 216']
           },
           {
             time: '3:00PM - 5:00PM',
             name: 'Puppies!!!!!',
             locations: ['Harrington Quad']
+          },
+          {
+            time: '4:30PM - 5:30PM',
+            name: 'API Workshop by Archana Ahlawat',
+            locations: ['Hudson 115A']
+          },
+          {
+            time: '4:45PM - 6:15PM',
+            name: 'Mobile Workshop by Co-Lab',
+            locations: ['Hudson 216']
           },
           {
             time: '6:30PM - 8:00PM',
@@ -122,6 +132,11 @@ export default class Schedule extends React.Component {
             time: '8:00PM - 9:00PM',
             name: 'MLH Werewolf',
             locations: ['CIEMAS 1st floor']
+          },
+          {
+            time: '9:00PM - 10:00PM',
+            name: 'Mixer for Female-Identifying and Non-Binary Hackers',
+            locations: ['Innovation Co-Lab']
           },
           {
             time: '9:00PM - 10:00PM',
@@ -154,9 +169,9 @@ export default class Schedule extends React.Component {
             locations: ['Schiciano Atrium']
           },
           {
-            time: '10:00AM - 12:00PM',
-            name: 'Workshop & Talks',
-            locations: ['Hudson']
+            time: '11:00AM - 12:00PM',
+            name: 'Turning Hack into Startup by Kasper Kubica',
+            locations: ['Hudson 222']
           },
           {
             time: '12:00PM - 1:30PM',
@@ -166,16 +181,24 @@ export default class Schedule extends React.Component {
           { time: '1:30PM', name: 'Hacking Ends' },
           { time: '2:00PM', name: 'Judging Begins' },
           {
-            time: '2:00PM - 3:30PM',
-            name: 'Judging / Demos',
-            locations: ['CIEMAS 1st Floor']
+            time: '2:00PM - 2:45PM',
+            name: 'First Round Judging',
+            locations: ['CIEMAS Lobby']
           },
           {
-            time: '3:45PM - 4:15PM',
-            name: 'Judging Finalists',
-            locations: ['CIEMAS 1st Floor']
+            time: '2:45PM - 3:30PM',
+            name: 'Finalist Demos',
+            locations: ['CIEMAS Lobby']
           },
-          { name: 'HackDuke Ends' },
+          {
+            time: '3:30PM - 4:00PM',
+            name: 'Final Judging',
+            locations: ['CIEMAS Lobby']
+          },
+          {
+            time: '4:00PM',
+            name: 'HackDuke Ends'
+          },
           {
             time: '4:30PM - 5:30PM',
             name: 'Closing Ceremony',
@@ -183,7 +206,7 @@ export default class Schedule extends React.Component {
           },
           {
             time: '6:00PM',
-            name: 'VA Bus Leaves',
+            name: 'VTech Bus Leaves',
             locations: ['Science Drive']
           }
         ]

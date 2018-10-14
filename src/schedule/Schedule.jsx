4be7c180c770e614b76dff672a04c7bb20b1996c
Yunyu Lin <mail@yunyul.in>
2018-10-13 22:14:58 -0400

    Move schedule-data to separate file

--- src/schedule/Schedule.jsx
+++ src/schedule/Schedule.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'react-emotion';
-import { css } from 'emotion';
 
 import {
   SectionBase,
@@ -15,6 +14,8 @@ import SectionTitle from '../SectionTitle';
 import ScheduleLine from './schedule-line.svg';
 import ScheduleLineExt from './schedule-line-ext.svg';
 import ScheduleItemCard from './ScheduleItemCard';
+
+import scheduleData from './schedule-data';
 
 const Container = styled('div')`
   ${SectionBase({
@@ -60,178 +61,15 @@ const formatDate = date =>
 
 export default class Schedule extends React.Component {
   state = {
-    selectedDayIndex: 0,
-    schedule: [
-      {
-        // Months are zero-indexed for whatever reason
-        // This is a historical artifact from the Java (yes, Java) APIs
-        date: new Date(2018, 9, 13),
-        events: [
-          {
-            time: '9:00AM - 10:45AM',
-            name: 'Check-In',
-            locations: ['Schiciano Atrium']
-          },
-          {
-            time: '11:00AM - 12:30PM',
-            name: 'Opening Ceremony',
-            locations: ['Reynolds Theatre']
-          },
-          {
-            time: '1:00PM - 2:00PM',
-            name: 'Lunch',
-            locations: ['Schiciano Atrium']
-          },
-          { time: '2:00PM', name: 'Hacking Commences' },
-          {
-            time: '2:00PM - 2:30PM',
-            name: 'Team Formation Mixer',
-            locations: ['Schiciano Lobby']
-          },
-          {
-            time: '2:00PM - 8:00PM',
-            name: 'Frisbee, Football, Volleyball',
-            locations: ['Harrington Quad']
-          },
-          {
-            time: '2:30PM - 3:30PM',
-            name: 'Intro to Hardware Hacking by August Ning',
-            locations: ['Hudson 201']
-          },
-          {
-            time: '2:30PM - 3:30PM',
-            name: 'VR Workshop by Mark Steelman',
-            locations: ['Hudson 232']
-          },
-            {
-            time: '3:00PM - 3:30PM',
-            name: 'Health Track Kickoff with Jason Liu',
-            locations: ['Hudson 222']
-          },
-          {
-            time: '3:00PM - 4:30PM',
-            name: 'Javascript Workshop by Co-Lab',
-            locations: ['Hudson 216']
-          },
-          {
-            time: '3:30PM - 5:00PM',
-            name: 'Puppies!!!!!',
-            locations: ['Harrington Quad']
-          },
-          {
-            time: '4:30PM - 5:30PM',
-            name: 'API Workshop by Archana Ahlawat',
-            locations: ['Hudson 115A']
-          },
-          {
-            time: '4:45PM - 6:15PM',
-            name: 'Mobile Workshop by Co-Lab',
-            locations: ['Hudson 216']
-          },
-          {
-            time: '6:30PM - 8:00PM',
-            name: 'Dinner',
-            locations: ['Schiciano Atrium']
-          },
-          {
-            time: '8:00PM - 9:00PM',
-            name: 'MLH Werewolf',
-            locations: ['Schiciano Lobby']
-          },
-          {
-            time: '9:00PM - 10:00PM',
-            name: 'Mixer for Female-Identifying and Non-Binary Hackers',
-            locations: ['Innovation Co-Lab']
-          },
-          {
-            time: '10:00PM - 11:00PM',
-            name: 'Nerf War',
-            locations: ["Twinnie's 2nd Floor"]
-          },
-          {
-            time: '11:00PM - 11:45PM',
-            name: 'Spicy Noodle Challenge',
-            locations: ['Schiciano Atrium']
-          }
-        ]
-      },
-      {
-        date: new Date(2018, 9, 14),
-        events: [
-          {
-            time: '12:00AM - 8:00AM',
-            name: 'Midnight Snacks',
-            locations: ["Twinnie's"]
-          },
-          {
-            time: '12:00AM - 8:00AM',
-            name: 'Nap Time',
-            locations: ["Hudson (222, 224, 232)"]
-          },
-          {
-            time: '9:00AM - 9:45AM',
-            name: 'Breakfast',
-            locations: ['Schiciano Atrium']
-          },
-            {
-            time: '11:00AM - 11:30PM',
-            name: 'The Future of Healthcare Technology by Dr. Michael Kerzner (Duke Hospital)',
-            locations: ['Hudson 216']
-          },
-          {
-            time: '11:00AM - 12:00PM',
-            name: 'Turning Hack into Startup by Kasper Kubica',
-            locations: ['Hudson 222']
-          },
-        
-          {
-            time: '12:00PM - 1:30PM',
-            name: 'Lunch',
-            locations: ['Schiciano Atrium']
-          },
-          { time: '1:30PM', name: 'Hacking Ends' },
-          { time: '2:00PM', name: 'Judging Begins' },
-          {
-            time: '2:00PM - 2:45PM',
-            name: 'First Round Judging',
-            locations: ['Schiciano Lobby']
-          },
-          {
-            time: '2:45PM - 3:30PM',
-            name: 'Finalist Demos',
-            locations: ['Schiciano Lobby']
-          },
-          {
-            time: '3:30PM - 4:00PM',
-            name: 'Final Judging',
-            locations: ['Schiciano Lobby']
-          },
-          {
-            time: '4:00PM',
-            name: 'HackDuke Ends'
-          },
-          {
-            time: '4:30PM - 5:30PM',
-            name: 'Closing Ceremony',
-            locations: ['Reynolds Theatre']
-          },
-          {
-            time: '6:00PM',
-            name: 'VTech Bus Leaves',
-            locations: ['Science Drive']
-          }
-        ]
-      }
-    ]
+    selectedDayIndex: 0
   };
 
   componentDidMount() {
-    const { schedule } = this.state;
     const now = new Date();
 
-    let i = schedule.length;
+    let i = scheduleData.length;
     while (i-- > 0) {
-      if (now >= schedule[i].date) {
+      if (now >= scheduleData[i].date) {
         this.setState({ selectedDayIndex: i });
         break;
       }
@@ -240,7 +78,7 @@ export default class Schedule extends React.Component {
 
   render() {
     const { id, titleColor } = this.props;
-    const { selectedDayIndex, schedule } = this.state;
+    const { selectedDayIndex } = this.state;
 
     return (
       <Container id={id}>
@@ -248,7 +86,7 @@ export default class Schedule extends React.Component {
         <SectionContent columns={true}>
           <SectionLeftColumn sticky={true}>
             <SectionTextContent>
-              {schedule.map(({ date, events }, i) => (
+              {scheduleData.map(({ date, events }, i) => (
                 <Day
                   active={i === selectedDayIndex}
                   activeColor={titleColor}
@@ -263,7 +101,7 @@ export default class Schedule extends React.Component {
           <SectionRightColumn>
             <ScheduleItemCard
               titleColor={titleColor}
-              events={schedule[selectedDayIndex].events}
+              events={scheduleData[selectedDayIndex].events}
             />
           </SectionRightColumn>
         </SectionContent>
---
+++ src/schedule/schedule-data.js
@@ -0,0 +1,163 @@
+export default [
+  {
+    // Months are zero-indexed for whatever reason
+    // This is a historical artifact from the Java (yes, Java) APIs
+    date: new Date(2018, 9, 13),
+    events: [
+      {
+        time: '9:00AM - 10:45AM',
+        name: 'Check-In',
+        locations: ['Schiciano Atrium']
+      },
+      {
+        time: '11:00AM - 12:30PM',
+        name: 'Opening Ceremony',
+        locations: ['Reynolds Theatre']
+      },
+      {
+        time: '1:00PM - 2:00PM',
+        name: 'Lunch',
+        locations: ['Schiciano Atrium']
+      },
+      { time: '2:00PM', name: 'Hacking Commences' },
+      {
+        time: '2:00PM - 2:30PM',
+        name: 'Team Formation Mixer',
+        locations: ['Schiciano Lobby']
+      },
+      {
+        time: '2:00PM - 8:00PM',
+        name: 'Frisbee, Football, Volleyball',
+        locations: ['Harrington Quad']
+      },
+      {
+        time: '2:30PM - 3:30PM',
+        name: 'Intro to Hardware Hacking by August Ning',
+        locations: ['Hudson 201']
+      },
+      {
+        time: '2:30PM - 3:30PM',
+        name: 'VR Workshop by Mark Steelman',
+        locations: ['Hudson 232']
+      },
+      {
+        time: '3:00PM - 3:30PM',
+        name: 'Health Track Kickoff with Jason Liu',
+        locations: ['Hudson 222']
+      },
+      {
+        time: '3:00PM - 4:30PM',
+        name: 'Javascript Workshop by Co-Lab',
+        locations: ['Hudson 216']
+      },
+      {
+        time: '3:30PM - 5:00PM',
+        name: 'Puppies!!!!!',
+        locations: ['Harrington Quad']
+      },
+      {
+        time: '4:30PM - 5:30PM',
+        name: 'API Workshop by Archana Ahlawat',
+        locations: ['Hudson 115A']
+      },
+      {
+        time: '4:45PM - 6:15PM',
+        name: 'Mobile Workshop by Co-Lab',
+        locations: ['Hudson 216']
+      },
+      {
+        time: '6:30PM - 8:00PM',
+        name: 'Dinner',
+        locations: ['Schiciano Atrium']
+      },
+      {
+        time: '8:00PM - 9:00PM',
+        name: 'MLH Werewolf',
+        locations: ['Schiciano Lobby']
+      },
+      {
+        time: '9:00PM - 10:00PM',
+        name: 'Mixer for Female-Identifying and Non-Binary Hackers',
+        locations: ['Innovation Co-Lab']
+      },
+      {
+        time: '10:00PM - 11:00PM',
+        name: 'Nerf War',
+        locations: ["Twinnie's 2nd Floor"]
+      },
+      {
+        time: '11:00PM - 11:45PM',
+        name: 'Spicy Noodle Challenge',
+        locations: ['Schiciano Atrium']
+      }
+    ]
+  },
+  {
+    date: new Date(2018, 9, 14),
+    events: [
+      {
+        time: '12:00AM - 8:00AM',
+        name: 'Midnight Snacks',
+        locations: ["Twinnie's"]
+      },
+      {
+        time: '12:00AM - 8:00AM',
+        name: 'Nap Time',
+        locations: ['Hudson (222, 224, 232)']
+      },
+      {
+        time: '9:00AM - 9:45AM',
+        name: 'Breakfast',
+        locations: ['Schiciano Atrium']
+      },
+      {
+        time: '11:00AM - 11:30PM',
+        name:
+          'The Future of Healthcare Technology by Dr. Michael Kerzner (Duke Hospital)',
+        locations: ['Hudson 216']
+      },
+      {
+        time: '11:00AM - 12:00PM',
+        name: 'Turning Hack into Startup by Kasper Kubica',
+        locations: ['Hudson 222']
+      },
+
+      {
+        time: '12:00PM - 1:30PM',
+        name: 'Lunch',
+        locations: ['Schiciano Atrium']
+      },
+      { time: '1:30PM', name: 'Hacking Ends' },
+      { time: '2:00PM', name: 'Judging Begins' },
+      {
+        time: '2:00PM - 2:45PM',
+        name: 'First Round Judging',
+        locations: ['Schiciano Lobby']
+      },
+      {
+        time: '2:45PM - 3:30PM',
+        name: 'Finalist Demos',
+        locations: ['Schiciano Lobby']
+      },
+      {
+        time: '3:30PM - 4:00PM',
+        name: 'Final Judging',
+        locations: ['Schiciano Lobby']
+      },
+      {
+        time: '4:00PM',
+        name: 'HackDuke Ends'
+      },
+      {
+        time: '4:30PM - 5:30PM',
+        name: 'Closing Ceremony',
+        locations: ['Reynolds Theatre']
+      },
+      {
+        time: '6:00PM',
+        name: 'VTech Bus Leaves',
+        locations: ['Science Drive']
+      }
+    ]
+  }
+];
